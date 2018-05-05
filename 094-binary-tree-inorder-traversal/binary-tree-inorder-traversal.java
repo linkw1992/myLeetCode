f34d206a3ff38a64748bc7b05f53acf86aca6edb
linkw1992 <38093287+linkw1992@users.noreply.github.com>
2018-05-05 13:29:08 +0800

    Add files via upload

---
+++ 094-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -0,0 +1,45 @@
+// Given a binary tree, return the inorder traversal of its nodes' values.
+//
+// Example:
+//
+//
+// Input: [1,null,2,3]
+//    1
+//     \
+//      2
+//     /
+//    3
+//
+// Output: [1,3,2]
+//
+// Follow up: Recursive solution is trivial, could you do it iteratively?
+//
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<Integer>();
+        Stack<TreeNode> stack=new Stack<TreeNode>();
+        TreeNode cur=root;//一个指针
+        while(cur!=null||!stack.isEmpty()){//stack不是null
+            while(cur!=null){
+                stack.push(cur);
+                cur=cur.left;
+                
+            }
+            cur=stack.pop();
+            res.add(cur.val);
+            cur=cur.right;
+        }
+        return res;
+    }
+}
---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,50 @@
+// Say you have an array for which the ith element is the price of a given stock on day i.
+//
+// Design an algorithm to find the maximum profit. You may complete at most k transactions.
+//
+// Note:
+// You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+//
+// Example 1:
+//
+//
+// Input: [2,4,1], k = 2
+// Output: 2
+// Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+//
+//
+// Example 2:
+//
+//
+// Input: [3,2,6,5,0,3], k = 2
+// Output: 7
+// Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+//              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+//
+//
+
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+        int len=prices.length;
+        if(k>=len/2){
+            int prof=0;
+            for(int i=1;i<len;i++){
+                if(prices[i]>prices[i-1])prof+=prices[i]-prices[i-1];
+            }
+                return prof;
+        }
+        int[] sell=new int[k+1];
+        int[] buy=new int[k+1];
+        for(int i=0;i<=k;i++){
+            buy[i]=Integer.MIN_VALUE;
+        }
+        for(int i=0;i<len;i++){
+            for(int j=1;j<=k;j++){
+                sell[j]=Math.max(sell[j],buy[j]+prices[i]);
+                buy[j]=Math.max(buy[j],sell[j-1]-prices[i]);
+            }
+        }
+            return sell[k];
+    }
+}
