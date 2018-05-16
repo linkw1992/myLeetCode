81ee991ee1d18af2aa354f3b4afced6a213b53eb
linkw1992 <38093287+linkw1992@users.noreply.github.com>
2018-05-17 00:28:43 +0800

    Add files via upload

---
+++ 273-integer-to-english-words/integer-to-english-words.java
@@ -0,0 +1,47 @@
+// Convert a non-negative integer to its english words representation. Given input is guaranteed to be less than 231 - 1.
+//
+// Example 1:
+//
+//
+// Input: 123
+// Output: "One Hundred Twenty Three"
+//
+//
+// Example 2:
+//
+//
+// Input: 12345
+// Output: "Twelve Thousand Three Hundred Forty Five"
+//
+// Example 3:
+//
+//
+// Input: 1234567
+// Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+//
+//
+
+
+class Solution {
+    String[] less20={"","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+    String[] less100={"","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] thousand={"","Thousand", "Million", "Billion"};
+    public String numberToWords(int num) {
+        if(num==0)return "Zero";
+        int i=0;
+        String s=new String();
+              while(num>0){
+                  if(num%1000!=0)
+                      s=towords(num%1000)+thousand[i]+" "+s;
+                  num/=1000;
+                  i++;
+              }
+        return s.trim();//with out space 
+    }
+    String towords(int num){//num<1000, remember the space
+        if(num==0)return "";
+        if(num<20)return less20[num]+" ";
+        if(num<100)return less100[num/10]+" "+towords(num%10);
+        return towords(num/100)+"Hundred "+towords(num%100);
+    }
+}
---
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.java
@@ -0,0 +1,42 @@
+//
+// Remove the minimum number of invalid parentheses in order to make the input string valid. Return all possible results.
+//
+// Note: The input string may contain letters other than the parentheses ( and ). 
+//
+//
+//
+// Examples:
+//
+// "()())()" -> ["()()()", "(())()"]
+// "(a)())()" -> ["(a)()()", "(a())()"]
+// ")(" -> [""]
+//
+//
+//
+// Credits:Special thanks to @hpplayer for adding this problem and creating all test cases.
+
+
+class Solution {
+    List<String> res=new ArrayList<>();
+    public List<String> removeInvalidParentheses(String s) {
+       dfs(s,0,0,new char[]{'(',')'});
+        return res;
+    }
+    void dfs(String s,int lasti,int lastj,char[] par){
+        int stack=0;
+        for(int i=lasti;i<s.length();i++){
+            if(s.charAt(i)==par[0])stack++;
+            if(s.charAt(i)==par[1])stack--;
+                if(stack<0){
+                    for(int j=lastj;j<=i;j++)
+                        if(s.charAt(j)==par[1] &&(j==lastj||s.charAt(j-1)!=par[1]))
+                            dfs(s.substring(0,j)+s.substring(j+1,s.length()),i,j,par);//substring without char(j)
+                        return;
+                }
+        }       
+        String rev=new StringBuilder(s).reverse().toString();
+        if(par[0]=='(')
+            dfs(rev,0,0, new char[]{')','('});
+            else res.add(rev);
+    }
+}
